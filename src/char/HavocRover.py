e4abd8a03edafc709b7ff2a32a8e9979be06f869
RU <cherish870424@gmail.com>
2025-02-16 02:41:15 +0800

    优化光主

--- src/char/HavocRover.py
+++ src/char/HavocRover.py
@@ -27,27 +27,22 @@ class HavocRover(BaseChar):
         elif self.Rover_Element == self.Elements[1]:
             if self.has_intro:
                 self.wait_intro()
-                self.n4(duration = 0.9)
-            else:
-                self.n4(duration = 0.6)
+            self.n4()
+            self.click_echo()
             forte_pass = False
             if self.click_liberation():
-                self.click_echo()
                 if not self.is_forte_full():
                     return self.switch_next_char()
                 else:
                     forte_pass = True
             if forte_pass or self.is_forte_full():
                 if self.click_resonance()[0]:
-                    self.click_echo()
                     self.continues_normal_attack(1.4)
                     return self.switch_next_char()
-            if self.click_resonance()[0]:
-                self.click_echo()
+            self.click_resonance()
             self.switch_next_char()
 
-    def n4(self, duration = 0.9):
+    def n4(self):
         self.heavy_attack()
-        #self.continues_normal_attack(duration)
         self.sleep(0.4)
         self.continues_normal_attack(0.8)
