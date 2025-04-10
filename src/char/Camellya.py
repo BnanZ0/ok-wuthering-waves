05b7ba07e8d3a8b9738eb45d3d937d8812aeca58
RU <cherish870424@gmail.com>
2025-04-10 22:34:06 +0800

    重构椿

--- src/char/Camellya.py
+++ src/char/Camellya.py
@@ -1,4 +1,5 @@
 import time
+from decimal import Decimal, ROUND_HALF_UP
 
 from src.char.BaseChar import BaseChar, Priority
 
@@ -8,9 +9,13 @@ class Camellya(BaseChar):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.last_heavy = 0
+        self.heavy_attack_timer = False
+        self.heavy_attack_timestamp = None
 
     def reset_state(self):
         super().reset_state()
+        self.heavy_attack_timer = False
+        self.heavy_attack_timestamp = None
 
     def do_get_switch_priority(self, current_char: BaseChar, has_intro=False, target_low_con=False):
         if has_intro:
@@ -42,15 +47,11 @@ class Camellya(BaseChar):
 
     def do_perform(self):
         if self.has_intro:
-            self.wait_intro(click=True)
-            start = time.time()
-            while time.time() - start < 8:
-                if self.is_con_full():
-                    break
-                self.click(interval=0.1)
-                self.check_combat()
+            self.continues_normal_attack(1.2)
+            self.sleep(0.1)
+            self.camellya_heavy_attack(4.1, until_con_full=True)
 
-        self.click_liberation(con_less_than=1)
+        self.click_liberation(con_less_than=0.82)
         start_con = self.get_current_con()
         if start_con < 0.82:
             loop_time = 1.1
@@ -62,41 +63,33 @@ class Camellya(BaseChar):
         while time.time() - budding_start_time < loop_time or self.task.find_one('camellya_budding', threshold=0.7):
             if not budding:
                 if self.ephemeral_ready():
-                    self.send_resonance_key()
+                    self.check_combat()
+                    self.send_resonance_key(post_sleep=1.2)
                     budding = True
                 else:
                     self.click(interval=0.1)
                     current_con = self.get_current_con()
                     if current_con < 0.82:
-                        self.send_resonance_key()
+                        self.click_resonance()
                         self.click_echo()
                         return self.switch_next_char()
                 if budding:
-                    self.sleep(1.2)
+                    self.logger.info(f'start budding')
+                    self.check_combat()
                     budding_start_time = time.time()
                     loop_time = 5.1
             else:
+                current_forte = self.get_camellya_forte(True)
                 if not heavy_att:
                     heavy_att = True
-                    self.task.send_key('space')
-                    if self.liberation_available():
-                        self.click_liberation()
-                    else:
-                        self.task.mouse_down(key='right')
-                        self.sleep(0.1,False)
-                        self.task.mouse_up(key='right')
-                        self.sleep(0.1,False)
                     self.task.mouse_down()
-                if self.liberation_available():
-                    self.click_liberation()
+                if self.click_liberation() and heavy_att:
                     self.sleep(0.2)
-                    if heavy_att:
-                        self.task.mouse_down()
-            if not self.task.in_combat():
-                if heavy_att:
-                    self.task.mouse_up()
-                self.task.raise_not_in_combat('combat check not in combat')
+                    self.task.mouse_down()
+            self.check_target(heavy_att)
             self.task.next_frame()
+            if heavy_att:
+                self.heavy_attack_check(current_forte, True)
         if heavy_att:
             self.task.mouse_up()
             self.sleep(0.1)
@@ -113,11 +106,80 @@ class Camellya(BaseChar):
     def ephemeral_ready(self):
         box = self.task.box_of_screen_scaled(3840, 2160, 3149, 1832, 3225, 1857, name='camellya_resonance', hcenter=True)
         red_percent = self.task.calculate_color_percentage(camellya_red_color, box)
-        self.logger.info(f'red_percent {red_percent}')
+        self.logger.debug(f'red_percent {red_percent}')
         return red_percent > 0.12    
     
+    def get_camellya_forte(self, budding=False):           
+        box = self.task.box_of_screen_scaled(3840, 2160, 1628, 2002, 2183, 2008, name='camellya_forte', hcenter=True)
+        forte_percent = 0
+        if not budding:
+            forte_percent = self.task.calculate_color_percentage(camellya_forte_color, box)
+            self.logger.debug(f'forte_percent {forte_percent}')
+        else:
+            forte_percent = self.task.calculate_color_percentage(camellya_budding_forte_color, box)
+            self.logger.debug(f'forte_percent_budding {forte_percent}')
+        forte_percent = Decimal(str(forte_percent)).quantize(Decimal('0.001'), rounding=ROUND_HALF_UP)
+        return forte_percent
+    
+    def camellya_heavy_attack(self, duration, check_combat = True, until_con_full = False):
+        self.task.mouse_down()
+        start = time.time()
+        while time.time() - start < duration:
+            current_forte = self.get_camellya_forte()
+            if current_forte <= 0.01:
+                break
+            if until_con_full and self.is_con_full():
+                break
+            if check_combat:
+                self.check_target(True)
+            self.task.next_frame()
+            self.heavy_attack_check(current_forte)
+        self.sleep(0.1)
+        self.task.mouse_up()
+        self.heavy_attack_timer = False
+
+    def heavy_attack_check(self, current_forte = None, budding = False):
+        if current_forte is None:
+            return
+        diff = current_forte - self.get_camellya_forte(budding)
+        self.logger.debug(f'diff {diff}')
+        if diff <= 0.002:
+            if not self.heavy_attack_timer:
+                self.heavy_attack_timer = True
+                self.heavy_attack_timestamp = time.time()
+        else:
+            self.heavy_attack_timer = False
+        if self.heavy_attack_timer and self.time_elapsed_accounting_for_freeze(self.heavy_attack_timestamp) > 0.7:
+            self.logger.debug(f'retry heavy attack')
+            self.task.mouse_up()
+            self.sleep(0.1)
+            self.task.mouse_down()
+            self.sleep(0.1)
+    
+    def check_target(self, heavy_att = False):
+        if not self.task.has_target():
+            if heavy_att:
+                self.task.mouse_up()
+            self.check_combat()
+            if heavy_att:
+                self.sleep(0.1)
+                self.task.mouse_down()
+                self.sleep(0.1)
+
 camellya_red_color = {
     'r': (200, 250),  # Red range
     'g': (60, 90),  # Green range
     'b': (150, 190)   # Blue range
-}  
+} 
+
+camellya_forte_color = {
+    'r': (225, 265),  # Red range
+    'g': (40, 68),  # Green range
+    'b': (125, 149)   # Blue range
+} 
+
+camellya_budding_forte_color = {
+    'r': (245, 265),  # Red range
+    'g': (185, 210),  # Green range
+    'b': (190, 225)   # Blue range
+} 
