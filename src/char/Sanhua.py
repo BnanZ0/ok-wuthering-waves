264610adfc4ff7ff7f661442a1d6498462427e1b
RU <cherish870424@gmail.com>
2025-04-11 12:13:10 +0800

    优化散华

--- src/char/Sanhua.py
+++ src/char/Sanhua.py
@@ -13,20 +13,23 @@ class Sanhua(BaseChar):
         if self.has_intro:
             sleep_time -= 0.1
             self.wait_intro(click=False, time_out=1.1)
-        if self.click_liberation(send_click=False):
-            sleep_time += 0.40
-            pass
         clicked_resonance, duration, _ = self.click_resonance(send_click=False)
-        clicked_echo = self.click_echo()
+        if clicked_resonance:
+            sleep_time -= duration
+            self.sleep(0.2)
+        clicked_liberation = self.click_liberation(send_click=False)
+        if clicked_liberation:
+            sleep_time += 0.4
         sleep_time -= self.time_elapsed_accounting_for_freeze(start)
         self.logger.debug('Sanhua to_sleep {}'.format(sleep_time))
         self.sleep(sleep_time)
         self.task.mouse_up()
-        if clicked_resonance and not clicked_echo:
-            after_sleep = 0.6
+        if clicked_resonance or clicked_liberation:
+            after_sleep = 1.1
         else:
             after_sleep = 0.3
-        if self.has_intro:
-            after_sleep += 0.8
         self.sleep(after_sleep)
+        if self.get_current_con() >= 0.99:
+            self.click_echo()
+            self.sleep(0.05)
         self.switch_next_char()
