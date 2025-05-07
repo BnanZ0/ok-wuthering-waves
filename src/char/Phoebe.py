95a9be4ee316fc44ad2d217b02700dc6e35af0d3
RU <cherish870424@gmail.com>
2025-05-07 20:09:56 +0800

    菲比初版

--- src/char/Phoebe.py
+++ src/char/Phoebe.py
@@ -10,6 +10,7 @@ class Phoebe(BaseChar):
         self.perform_outro_time = 0
         self.resonance_time = 0
         self.attribute = 0
+        self.star_available = False
         self.char_zani = None
         self.starflash_combo_count = 0
         self.is_action_complete = False
@@ -20,6 +21,7 @@ class Phoebe(BaseChar):
         self.perform_outro_time = 0
         self.resonance_time = 0
         self.attribute = 0
+        self.star_available = False
         self.char_zani = None
         self.starflash_combo_count = 0
         self.is_action_complete = False
@@ -38,24 +40,22 @@ class Phoebe(BaseChar):
             self.continues_normal_attack(0.1)
             return self.switch_next_char()
         
-        if not self.first_liberation or not self.in_absolution_or_confession():
-            self.logger.debug('wait for UI')
-            # 0.4秒让UI载入
-            wait_ui_time = 0.4 - (time.time() - start)
-            if wait_ui_time > 0:
-                self.continues_normal_attack(wait_ui_time)
-        
-        if (self.attribute == 2 
-            and self.char_zani is not None 
-            and (self.char_zani.in_liberation or self.char_zani.liberation_allowed)
-        ):
-            self.logger.debug('zani in liberation')
+        if self.is_zani_ready():
+            self.logger.debug('stop applying spectro frazzle')
             if not self.char_zani.liberation_elapsed() > 18:
                 if self.resonance_available():
                     self.click_resonance_once()
                 else:
                     self.continues_normal_attack(1.4)
             return self.switch_next_char()
+        
+        if not self.check_middle_star():
+            self.absolution_or_confession()
+        elif not self.in_absolution_or_confession():
+            self.logger.debug('wait for UI')
+            wait_ui_time = 0.4 - (time.time() - start)
+            if wait_ui_time > 0:
+                self.continues_normal_attack(wait_ui_time)
         
         if self.attribute == 1:
             self.click_echo()
@@ -68,7 +68,11 @@ class Phoebe(BaseChar):
         if self.resonance_available():
             self.logger.debug('click_resonance_once')
             if self.resonance_time > 0 and self.all_time_elapsed_accounting_for_freeze(self.resonance_time) <= 11.4:
-                self.click_resonance()        
+                self.update_current_status()
+                if self.is_action_complete:
+                    self.click_resonance() 
+                else:
+                    self.starflash_combo()       
             else:
                 self.click_resonance_once()
                 self.starflash_combo()
@@ -102,7 +106,7 @@ class Phoebe(BaseChar):
     def perform_heavy_attack(self):
         if not self.absolution_or_confession():
             start = time.time()
-            while self.is_forte_full() or time.time() - start < 0.6:
+            while self.is_forte_full():
                 self.check_combat()
                 self.heavy_attack(duration=0.3)
                 if time.time() - start > 2.5:
@@ -121,14 +125,20 @@ class Phoebe(BaseChar):
     
     def absolution_or_confession(self):
         self.task.wait_in_team_and_world(time_out=3, raise_if_not_found=False)
-        if self.confession_ready():
+        condition = lambda: False
+        if not self.star_available and self.is_forte_full():
+            condition = lambda: self.is_forte_full()
+        elif self.confession_ready():
+            condition = lambda: self.confession_ready()
+
+        if condition():
             if self.attribute == 2:
                 self.task.send_key_down(self.get_resonance_key())
             else:
                 self.task.mouse_down()
                 
             start = time.time()
-            while self.confession_ready() or time.time() - start < 0.6:
+            while condition() or time.time() - start < 0.5:
                 if time.time() - start > 1.2:
                     break
                 self.task.next_frame()
@@ -139,7 +149,8 @@ class Phoebe(BaseChar):
             else:
                 self.task.mouse_up()
                 self.logger.info(f'Enters absolution status')
-
+            
+            self.star_available = True
             self.first_liberation = True
             self.continues_right_click(0.1)
             self.starflash_combo_count = 0
@@ -173,20 +184,14 @@ class Phoebe(BaseChar):
         return forte_percent > 0.03
 
     def update_current_status(self):
-        if (self.attribute == 2 
-            and self.char_zani is not None 
-            and (self.char_zani.in_liberation or self.char_zani.liberation_allowed)
-        ):
-            return
         if self.attribute == 2:
             times = 2
         else:
             times = 4
         if (self.first_liberation 
-            and self.starflash_combo_count >= times
+            and self.starflash_combo_count == times
+            and not self.liberation_available()
         ):
-            if self.liberation_available():
-                self.click_liberation()
             self.is_action_complete = True
         else:
             self.is_action_complete = False
@@ -197,8 +202,20 @@ class Phoebe(BaseChar):
     def has_long_actionbar(self):
         return True
         
+    def is_zani_ready(self):
+        if self.attribute == 2 and self.char_zani is not None:
+            if (self.char_zani.in_liberation 
+                or self.char_zani.liberation_allowed
+                or self.char_zani.blaze >= self.char_zani.blaze_threshold
+            ):
+                return True
+        return False
+    
     def switch_next_char(self, *args):
-        self.update_current_status()
+        if not self.is_zani_ready():
+            if self.liberation_available():
+                self.click_liberation()
+            self.update_current_status()
         if self.is_con_full():
             self.click_echo()
             self.sleep(0.05)
@@ -224,12 +241,12 @@ class Phoebe(BaseChar):
             return True
         box = self.task.box_of_screen_scaled(3840, 2160, 1890, 2010, 1930, 2030, name='phoebe_middle_star', hcenter=True)
         forte_percent = self.task.calculate_color_percentage(phiebe_star_light_cloor, box)
-        self.logger.info(f'middle_star_light_percent {forte_percent}')
+        self.logger.debug(f'middle_star_light_percent {forte_percent}')
         if forte_percent > 0.1:
             self.star_available = True
             return True
         forte_percent = self.task.calculate_color_percentage(phiebe_star_blue_cloor, box)
-        self.logger.info(f'middle_star_blue_percent {forte_percent}')
+        self.logger.debug(f'middle_star_blue_percent {forte_percent}')
         if forte_percent > 0.1:
             self.star_available = True
             return True    
