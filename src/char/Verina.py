d1aa4ade93f12c4ed43e28af51c26ff02b653e7e
RU <cherish870424@gmail.com>
2025-05-19 14:18:52 +0800

    1.调整维里奈liberation_priority 2.优化维里奈流程 3.修正bug

--- src/char/Verina.py
+++ src/char/Verina.py
@@ -2,15 +2,21 @@ from src.char.Healer import Healer
 
 
 class Verina(Healer):
-
+    def count_liberation_priority(self):
+        return 2
+    
     def do_perform(self):
-        self.wait_intro(click=False, time_out=1.1)
-        liberated = self.click_liberation()
+        if self.has_intro:
+            self.wait_intro(click=False, time_out=1.1)
+        else:
+            self.sleep(0.01)
         if self.flying():
+            self.logger.info('Verina flying')
             self.normal_attack()
             return self.switch_next_char()
         self.click_resonance()
         self.click_echo()
+        liberated = self.click_liberation()
         if self.is_forte_full():
             self.heavy_attack()
         elif not liberated:
